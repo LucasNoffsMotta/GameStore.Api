1ef7a1ed6f2c55b17c9aa7b62557f305072ae9d2
Lucas Motta <lnoffs@hotmail.com>
2025-04-02 19:02:29 -0300

    Updates

--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fbd2a41f4f8f8fbdf939178bca0b433537329d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5ec161d5ecbd0a405569c401ca56a1204cee0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
